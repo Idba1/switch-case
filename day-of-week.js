0cfa6e6f27c45aa6f9c92f89145fadb92843beae
Idba1 <moniraislamself@gmail.com>
2024-01-28 04:47:46 +0600

    day-of-week.js

---
+++ day-of-week.js
@@ -0,0 +1,26 @@
+const dayOfWeek = 5;
+switch(dayOfWeek){
+    case 1:
+        console.log("Satureday");
+        break;
+    case 2:
+        console.log("Sunday");
+        break;
+    case 3:
+        console.log("Monday");
+        break;
+    case 4:
+        console.log("Tuesday");
+        break;
+    case 5:
+        console.log("Wednesday");
+        break;
+    case 6:
+        console.log("Thusday");
+        break;
+    case 7:
+        console.log("Friday");
+        break;
+    default:
+        console.log("invalid input");
+}
