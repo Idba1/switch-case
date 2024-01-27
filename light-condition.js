c9b9ae8782fa652f54a1c017882bae04cf2f6a40
Idba1 <moniraislamself@gmail.com>
2024-01-28 04:52:22 +0600

    light-condition.js

---
+++ light-condition.js
@@ -0,0 +1,11 @@
+const lightCondition = 0;
+switch (lightCondition){
+    case 1:
+        console.log("Light on");
+        break;
+    case 0:
+        console.log("Light off");
+        break;
+    default: 
+        console.log("input is invalid");
+}
